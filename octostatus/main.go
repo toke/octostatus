1e3f33301bc6330894e64b105a4cd616095f8442
Thomas Kerpe <toke@toke.de>
2017-08-04 10:49:13 +0200

    removed duplicates

--- octostatus/main.go
+++ octostatus/main.go
@@ -14,43 +14,6 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
-type JobInfo struct {
-	JobDetail JobDetail   `json:"job"`
-	Progress  JobProgress `json:"progress"`
-	State     string      `json:"state"`
-}
-
-type JobDetail struct {
-	AveragePrintTime   float64                `json:"averagePrintTime"`
-	EstimatedPrintTime float64                `json:"estimatedPrintTime"`
-	LastPrintTime      float64                `json:"lastPrintTime"`
-	File               JobFile                `json:"file"`
-	Tool               map[string]JobFilament `json:"filament"`
-}
-
-type JobFile struct {
-	Date   int    `json:"date"`
-	Name   string `json:"name"`
-	Origin string `json:"origin"`
-	Path   string `json:"path"`
-	Size   int32  `json:"size"`
-}
-
-type JobFilament struct {
-	Length float64 `json:"length"`
-	Volume float64 `json:"volume"`
-}
-
-type JobProgress struct {
-	ETA                 string  `json:"ETA,omitempty"`
-	Completion          float64 `json:"completion,omitempty"`
-	Filepos             int32   `json:"filepos"`
-	PrintTime           int32   `json:"printTime"`
-	PrintTimeLeft       int32   `json:"printTimeLeft"`
-	PrintTimeLeftOrigin string  `json:"printTimeLeftOrigin,omitempty"`
-	PrintTimeLeftString string  `json:"printTimeLeftString"`
-}
-
 type Config struct {
 	Name    string                   `yaml:"name"`
 	Version int16                    `yaml:"version"`
